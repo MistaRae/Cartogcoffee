d6fee4a718f8d00476ec6297a00232401e9ed65f
Aaron McKie <aaron.mckie@icloud.com>
2021-05-21 15:52:51 +0100

    more shits working, crud functionality up

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 const MongoClient = require('mongodb').MongoClient
 const createRouter = require ('./helpers/create_router.js');
 
 MongoClient.connect('mongodb://localhost:27017')
-
+.then((client) => {
+    const db = client.db('coffee_facts');
+    const coffeesCollection = db.collection('coffees');
+    const coffeesRouter = createRouter(coffeesCollection);
+    app.use('/api/coffees', coffeesRouter);
+  })
+  .catch(console.err);
 
 app.listen(5000, function () {
     console.log(`listening on port: ${this.address().port}`);
