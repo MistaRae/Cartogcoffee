2e466ea67785ab702c33192ee95469e933d9a810
Daniel Callus <danielcallus@gmail.com>
2021-05-21 08:13:57 +0100

    basic leaflet working, geojson installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import L from 'leaflet';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
-function App() {
+// type State = {
+//   lat: Number,
+//   lng: Number,
+//   zoom: Number,
+// }
+
+var myIcon = L.icon({
+  iconUrl: 'https://www.pinclipart.com/picdir/big/130-1303440_current-location-location-map-nearby-pin-pointer-slider.png',
+  iconSize: [25, 41],
+  iconAnchor: [12.5, 41],
+  popupAnchor: [0, -41]
+});
+
+
+class App extends Component {
+
+  state = {
+    lat: 51.505,
+    lng: 0.09,
+    zoom: 13,
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <MapContainer className="map" attributionControl={false} center={position} zoom={this.state.zoom} >
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position} icon={myIcon}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
+  }
 }
 
 export default App;
