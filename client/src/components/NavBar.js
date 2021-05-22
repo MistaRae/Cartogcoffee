bc470b42cbae80f89f009446585fb4a8e94ef35b
Daniel Callus <danielcallus@gmail.com>
2021-05-22 17:54:16 +0100

    added filters to dropdown menu

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const NavBar = ({coffees, onSelectRegion}) => {
+const NavBar = ({ coffees, onSelectRegion }) => {
 
     const handleSelectChange = function (event) {
         let regionObject = coffees[event.target.value];
@@ -12,16 +12,35 @@ const NavBar = ({coffees, onSelectRegion}) => {
         return <option value={index} key={index}>{coffee.region}</option>
     })
 
-    return ( 
-        <div id = "navbar-container">
-            <select onChange={handleSelectChange} class = "drop-down" name="field-1" id="field-1">
+    // foobar = foo.split(",").map(foo => foo.trim())
+
+    const getFlavours = () => {
+
+        let flavourList = []
+        const profileList = coffees.map((coffee, index) => {
+            const flavours = coffee.taste_profile.split(",").map(taste => taste.trim());
+            flavourList.push(...flavours)
+        });
+        const flavourSet = new Set(flavourList);
+        const flavourArray = Array.from(flavourSet);
+        return flavourArray
+    }
+
+    const flavourMapping = getFlavours().map((flavour, index) => {
+        return <option value={index} key={index}>{flavour}</option>
+    })
+
+    // return <option value={index} key={index}>{coffee.region}</option>
+
+    return (
+        <div id="navbar-container">
+            <select onChange={handleSelectChange} class="drop-down" name="field-1" id="field-1">
                 {regionList}
             </select>
-            <select class = "drop-down" name="field-2" id="field-2">
-                <option value="option1">option 1</option>
-                <option value="option1">option 2</option>
+            <select class="drop-down" name="field-2" id="field-2">
+                {flavourMapping}
             </select>
-            <select class = "drop-down" name="field-3" id="field-3">
+            <select class="drop-down" name="field-3" id="field-3">
                 <option value="option1">option 1</option>
                 <option value="option1">option 2</option>
             </select>
