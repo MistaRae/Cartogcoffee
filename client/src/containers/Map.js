14b58ec2c2d5f4319bf442bb3aae909698cdb454
Daniel Callus <danielcallus@gmail.com>
2021-05-22 16:22:29 +0100

    changing from africa to americas

--- client/src/containers/Map.js
+++ client/src/containers/Map.js
@@ -43,7 +43,9 @@ const Map = () => {
     const map = useMap()
     if (selectedRegion === "Americas")  {
       map.flyTo([-26, -60], 3.2)
-      
+    }
+    else if (selectedRegion === "Africa")  {
+      map.flyTo([5, 13], 3.4)
     }
     return null
   }
