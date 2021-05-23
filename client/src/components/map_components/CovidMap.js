4be535ee54466df84bbf41ce40c71a45f947d606
Daniel Callus <danielcallus@gmail.com>
2021-05-23 11:52:27 +0100

    updated css

--- client/src/components/map_components/CovidMap.js
+++ client/src/components/map_components/CovidMap.js
@@ -4,9 +4,17 @@ import "leaflet/dist/leaflet.css";
 import "./CovidMap.css"
 
 const CovidMap = ({ countries }) => {
-    console.log(countries);
+    
+    const mapStyle = {
+        fillColor: "white",
+        weight: 0.3,
+        color: "black",
+        fillOpacity: 1
+    };
+
+
     return ( <MapContainer style={{ height: "50vh"}} zoom={2} center={[20, 100]}>
-            <GeoJSON data={countries} />
+            <GeoJSON data={countries} style={mapStyle} />
 
     </MapContainer>
     
