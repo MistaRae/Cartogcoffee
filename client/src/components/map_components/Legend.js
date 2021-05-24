1f65f1a1b2a74fe4136b0768b6dbd830c789e50b
Daniel Callus <danielcallus@gmail.com>
2021-05-24 20:03:56 +0100

    fixed legend

--- client/src/components/map_components/Legend.js
+++ client/src/components/map_components/Legend.js
@@ -14,19 +14,22 @@ const Legend = ({ legend}) => {
         backgroundColor: "#000000"
       }}
     >
-      <span style={{backgroundColor: "#ffffff", padding: "0 5% 0 5%"}}> 
+      <span style={{backgroundColor: "#ffffff", padding: "0 4% 0 4%"}}> 
       {legend[4]}
       </span>
-      <span style={{backgroundColor: "#d88383", padding: "0 5% 0 5%"}}>0 -{'\u00A0'}
+      <span style={{backgroundColor: "#d88383", padding: "0 4% 0 4%"}}>0 -{'\u00A0'}
         {formatNumber(legend[3])}
         </span>
-      <span style={{backgroundColor: "#c57f7f", padding: "0 5% 0 5%"}}>
+        <span style={{backgroundColor: "#c47f7f", padding: "0 4% 0 4%"}}>
+        {formatNumber(legend[3])} - {formatNumber(legend[2] - 1)}
+        </span>
+        <span style={{backgroundColor: "#c47f7f", padding: "0 4% 0 4%"}}>
         {formatNumber(legend[2])} - {formatNumber(legend[1] - 1)}
         </span>
-      <span style={{backgroundColor: "#9c2929", padding: "0 5% 0 5%", color: "white"}}>
+      <span style={{backgroundColor: "#9c2929", padding: "0 4% 0 4%", color: "white"}}>
         {formatNumber(legend[1])} - {formatNumber(legend[0] - 1)}
         </span>
-      <span style={{backgroundColor: "#741f1f", padding: "0 5% 0 5%", color: "white"}}>
+      <span style={{backgroundColor: "#741f1f", padding: "0 4% 0 4%", color: "white"}}>
         {formatNumber(legend[0])}+
         </span>
       <span style={{color: "white"}}> ({legend[5]})</span>
