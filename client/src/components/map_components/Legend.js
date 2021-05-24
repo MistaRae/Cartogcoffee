d6ff74e8bb281155dedc7577eab4414d8400e310
Daniel Callus <danielcallus@gmail.com>
2021-05-24 19:57:58 +0100

    legend now automates numbers

--- client/src/components/map_components/Legend.js
+++ client/src/components/map_components/Legend.js
@@ -1,19 +1,35 @@
 import React from "react";
 
-const Legend = ({ legend, legendItems }) => {
+const Legend = ({ legend}) => {
+
+  const formatNumber = (number) =>  {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
   return (
     <div
       style={{
         display: "flex",
-        alignItems: "stretch",
+        alignItems: "center",
+        justifyContent: "space-around",
+        backgroundColor: "#000000"
       }}
     >
-      <span> {legend[4]}</span>
-      <span>{legend[3]}</span>
-      <span>{legend[2]}</span>
-      <span>{legend[1]}</span>
-      <span>{legend[0]}</span>
-      <span> {legend[5]}</span>
+      <span style={{backgroundColor: "#ffffff", padding: "0 5% 0 5%"}}> 
+      {legend[4]}
+      </span>
+      <span style={{backgroundColor: "#d88383", padding: "0 5% 0 5%"}}>0 - 
+        {formatNumber(legend[3])}
+        </span>
+      <span style={{backgroundColor: "#c57f7f", padding: "0 5% 0 5%"}}>
+        {formatNumber(legend[2])} - {formatNumber(legend[1] - 1)}
+        </span>
+      <span style={{backgroundColor: "#9c2929", padding: "0 5% 0 5%", color: "white"}}>
+        {formatNumber(legend[1])} - {formatNumber(legend[0] - 1)}
+        </span>
+      <span style={{backgroundColor: "#741f1f", padding: "0 5% 0 5%", color: "white"}}>
+        {formatNumber(legend[0])}+
+        </span>
+      <span style={{color: "white"}}> ({legend[5]})</span>
 
     </div>
   );
