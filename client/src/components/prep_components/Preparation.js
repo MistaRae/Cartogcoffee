9bbba5c7f17e76dce488ec586a7f3db4e6c22a47
MistaRae <itsamistarae@gmail.com>
2021-05-25 13:03:51 +0100

    added footer padding

--- client/src/components/prep_components/Preparation.js
+++ client/src/components/prep_components/Preparation.js
@@ -6,7 +6,8 @@ const Preparation = () => {
 
     return(
         <div id = "prep">
-            <img id = "prep-image" src="https://images.unsplash.com/photo-1455134168668-4066045897c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80" alt="" />
+            {/* <img id = "prep-image" src="https://images.unsplash.com/photo-1455134168668-4066045897c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80" alt="" /> */}
+            <Footer/>
         </div>
     )
 
