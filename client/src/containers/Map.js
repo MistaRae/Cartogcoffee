c6220382a85e6fc2a2cfb647db9d2abdf7620d8b
Daniel Callus <danielcallus@gmail.com>
2021-05-25 20:51:54 +0100

    bounds set

--- client/src/containers/Map.js
+++ client/src/containers/Map.js
@@ -136,7 +136,9 @@ const Map = ({coffees}) => {
           />
 
 
-          <MapContainer className="map" attributionControl={false} center={positionCenter} zoom={state.zoom} >
+          <MapContainer className="map" attributionControl={false} center={positionCenter} zoom={state.zoom}
+          maxBounds={[[100, 100], [-100, -100]]}
+          scrollWheelZoom={false} >
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
